a2f21e69205e0a374227e3bea0b99215a6886bf7
john <johntang1026@gmail.com>
2019-09-16 10:37:25 +0800

    temp commit

--- src/common/api.js
+++ src/common/api.js
@@ -5,6 +5,18 @@ import wepy from '@wepy/core';
 // console.log(wepy);
 const host = 'http://0.0.0.0:3000'
 
+
+const fetchTokenHeader = () =>{
+  return new Promise((resolve, reject) => {
+    wepy.wx.getStorage({key: 'accessToken'})
+      .then(res => resolve({
+        'content-type': 'application/json',
+        'Accept':'*/*',
+        'Authorization': res.data
+      })).catch(err => eventHub.$emit('wx-error', err))
+  })
+}
+
 const authenticate = () => {
   return new Promise((resolve, reject) => { 
     wepy.wx.login().then((res) => {
@@ -22,6 +34,50 @@ const authenticate = () => {
   });
 }
 
+const updateUser = (profiles) => {
+  return new Promise((resolve, reject) => {    
+    fetchTokenHeader().then((header) => {
+      request({
+        url: host + "/v1/profiles",
+        data: {profiles: profiles},
+        header: header,
+        method: 'PUT'
+      }).then((res) => {
+        resolve(res);
+      }).catch((res) => {
+        reject(res);
+      })
+    })
+  })
+
+}
+
+const fetchUser = () => {
+  return new Promise((resolve, reject) => {    
+    fetchTokenHeader().then((header) => {
+      request({
+        url: host + '/v1/profiles',
+        header: header,
+        method: 'GET'
+      }).then((res) => {
+        resolve(res)
+      }).catch((err)=>{
+        eventHub.$emit('wx-error', err)
+      })
+    })
+  })
+}
+
+const createPost = (data) => {
+  return new Promise((resolve, reject) => {
+    request({
+      url: host + ''
+    })
+  })
+} 
 module.exports = {
-  authenticate: authenticate
+  authenticate: authenticate,
+  createPost: createPost,
+  fetchUser: fetchUser,
+  updateUser: updateUser,
 }
