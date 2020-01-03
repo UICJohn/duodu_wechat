66151091a480928c927af700b59345df006bd97b
john <johntang1026@gmail.com>
2020-01-03 16:08:50 +0800

    tweak api lib

--- src/common/api.js
+++ src/common/api.js
@@ -2,29 +2,18 @@ import { request } from './utils'
 import eventHub from './eventHub';
 import wepy from '@wepy/core';
 
-const fetchTokenHeader = () =>{
-  return new Promise((resolve, reject) => {
-    wepy.wx.getStorage({key: 'accessToken'})
-      .then(res => resolve({
-        'content-type': 'application/json',
-        'Accept':'*/*',
-        'Authorization': res.data
-      })).catch( (err) => {
-        eventHub.$emit('auth-user', err);
-      })
-  })
-}
-
 const authenticate = () => {
   return new Promise((resolve, reject) => { 
     wepy.wx.login().then((res) => {
       request({
         url: BASE_URL + '/users/wechat_auth',
         method: 'POST',
-        data: { code: res.code },
-        header: {'content-type': 'application/json'}
-      }).then((res) => {
+        data: { code: res.code }
+      }, false).then((res) => {
         resolve(res)
+      }).catch((err) => {
+        eventHub.$emit('wx-error', err);
+        reject(err);
       })
     }).catch((err) => { 
       eventHub.$emit('wx-error', err);
@@ -35,94 +24,80 @@ const authenticate = () => {
 
 const sendCode = (data) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + "/v1/verification_code",
-        method:'POST',
-        data: data,
-        header: header
-      }).then((res) => {
-        resolve(res);
-      })
+    request({
+      url: BASE_URL + "/v1/verification_code",
+      method:'POST',
+      data: data,
+      header: header
+    }).then((res) => {
+      resolve(res);
     })
   })
 }
 
-
 const updateEmail = (data) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + "/v1/profiles/update_email",
-        method: 'PUT',
-        data: data,
-        header: header
-      }).then(res => {
-        resolve(res);
-      })
+    request({
+      url: BASE_URL + "/v1/profiles/update_email",
+      method: 'PUT',
+      data: data,
+      header: header
+    }).then(res => {
+      resolve(res);
     })
   })
 }
 
 const fetchSuburbs = (region) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + "/v1/suburbs",
-        method: 'GET',
-        data: {region: region},
-        header: header
-      }).then(res => {
-        resolve(res.data.suburbs)
-      })
+    request({
+      url: BASE_URL + "/v1/suburbs",
+      method: 'GET',
+      data: {region: region},
+      header: header
+    }).then(res => {
+      resolve(res.data.suburbs)
     })
   })
 }
 
 const updatePhone = (data) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + "/v1/profiles/update_phone",
-        method: 'PUT',
-        data: data,
-        header: header
-      }).then(res => {$
-        resolve(res);
-      })
+    request({
+      url: BASE_URL + "/v1/profiles/update_phone",
+      method: 'PUT',
+      data: data,
+      header: header
+    }).then(res => {$
+      resolve(res);
     })
   })
 }
 
 const uploadPostImage = (post_id, image_path, data={}) => {
   return new Promise((resolve, reject) => { 
-    fetchTokenHeader().then((header) => {
-      wepy.wx.uploadFile({
-        url: BASE_URL + "/v1/posts/" + post_id + "/upload_images",
-        filePath: image_path,
-        name: 'attachment',
-        formData: data,
-        header: header
-      }).then(res => {
-        resolve(res)
-      }).catch(err => {
-        reject(err)
-      })
+    wepy.wx.uploadFile({
+      url: BASE_URL + "/v1/posts/" + post_id + "/upload_images",
+      filePath: image_path,
+      name: 'attachment',
+      formData: data,
+      header: header
+    }).then(res => {
+      resolve(res)
+    }).catch(err => {
+      reject(err)
     })
   })
 }
 
 const updateUser = (profiles) => {
   return new Promise((resolve, reject) => {    
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + "/v1/profiles",
-        data: {profiles: profiles},
-        header: header,
-        method: 'PUT'
-      }).then((res) => {
-        resolve(res);
-      })
+    request({
+      url: BASE_URL + "/v1/profiles",
+      data: {profiles: profiles},
+      method: 'PUT'
+    }).then((res) => {
+      resolve(res);
     })
   })
 
@@ -130,66 +105,54 @@ const updateUser = (profiles) => {
 
 const fetchUser = () => {
   return new Promise((resolve, reject) => {    
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + '/v1/profiles',
-        header: header,
-        method: 'GET'
-      }).then((res) => {
-        resolve(res)
-      }).catch((err)=>{
-        eventHub.$emit('wx-error', err)
-      })
+    request({
+      url: BASE_URL + '/v1/profiles',
+      method: 'GET'
+    }).then((res) => {
+      resolve(res)
+    }).catch((err)=>{
+      eventHub.$emit('wx-error', err)
     })
   })
 }
 
 const fetchSchools = (key) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + "/v1/schools",
-        header: header,
-        data: {key: key},
-        method: 'GET'
-      }).then(res => {
-        resolve(res.data.schools);
-      }).catch(res => {
-        reject(res);
-      })
+    request({
+      url: BASE_URL + "/v1/schools",
+      data: {key: key},
+      method: 'GET'
+    }).then(res => {
+      resolve(res.data.schools);
+    }).catch(res => {
+      reject(res);
     })
   })
 }
 
 const createPost = (data) => {
   return new Promise((resolve, reject)=>{
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + '/v1/posts',
-        header: header,
-        data: { post: data },
-        method: 'POST'
-      }).then(res => {
-        resolve(res.data.post);
-      }).catch(err => {
-        reject(err);
-      })
+    request({
+      url: BASE_URL + '/v1/posts',
+      data: { post: data },
+      method: 'POST'
+    }).then(res => {
+      resolve(res.data.post);
+    }).catch(err => {
+      reject(err);
     })
   })
 }
 
 const fetchPosts = (filters = {}) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header)=>{
-      request({
-        url: BASE_URL + '/v1/posts',
-        header: header,
-        method: 'GET'
-      }).then(res => {
-        resolve(res.data.posts);
-      }).catch(err => {
-        reject(err);
-      })
+    request({
+      url: BASE_URL + '/v1/posts',
+      method: 'GET'
+    }).then(res => {
+      resolve(res.data.posts);
+    }).catch(err => {
+      reject(err);
     })
   })
 }
@@ -197,93 +160,77 @@ const fetchPosts = (filters = {}) => {
 
 const fetchPost = (id) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header)=>{
-      request({
-        url: BASE_URL + '/v1/posts/' + id,
-        header: header,
-        method: 'GET'
-      }).then(res => {
-        resolve(res.data.post);
-      })
+    request({
+      url: BASE_URL + '/v1/posts/' + id,
+      method: 'GET'
+    }).then(res => {
+      resolve(res.data.post);
     })
   })
 }
 
 const fetchSubways = (city) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + '/v1/subways',
-        data: {region: city},
-        header: header,
-        method: 'GET'
-      }).then(res => {
-        resolve(res.data.subways);
-      }).catch(err => {
-        reject(err);
-      })
+    request({
+      url: BASE_URL + '/v1/subways',
+      data: {region: city},
+      method: 'GET'
+    }).then(res => {
+      resolve(res.data.subways);
+    }).catch(err => {
+      reject(err);
     })
   })
 }
 
 const likePost = (post_id, like = true) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      let url = like ? "/v1/posts/"+post_id+"/like" :  "/v1/posts/"+post_id+"/dislike"
-      let method = like ? "POST" : "DELETE"
-      request({
-        url: BASE_URL + url,
-        header: header,
-        method: method
-      }).then(res => {
-        resolve(res.data.subways);
-      }).catch(err => {
-        reject(err);
-      })
+    let url = like ? "/v1/posts/"+post_id+"/like" :  "/v1/posts/"+post_id+"/dislike"
+    let method = like ? "POST" : "DELETE"
+    request({
+      url: BASE_URL + url,
+      method: method
+    }).then(res => {
+      resolve(res.data.subways);
+    }).catch(err => {
+      reject(err);
     })
-  }) 
+  })
 }
 
 const postComment = (postId, comment) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + '/v1/post_comments',
-        header: header,
-        method: 'POST',
-        data: {
-          comment: {
-            post_id: postId,
-            body: comment
-          }
+    request({
+      url: BASE_URL + '/v1/post_comments',
+      method: 'POST',
+      data: {
+        comment: {
+          post_id: postId,
+          body: comment
         }
-      }).then(res => {
-        console.log(res);
-        resolve(res);
-      }).catch(err => {
-        reject(err);
-      })
+      }
+    }).then(res => {
+      resolve(res);
+    }).catch(err => {
+      reject(err);
     })
   })
 }
 
 const replyComment = (commentId, comment) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + '/v1/post_comments/' + commentId + '/reply',
-        header: header,
-        method: 'POST',
-        data: {
-          comment: {
-            body: comment
-          }
+    request({
+      url: BASE_URL + '/v1/post_comments/' + commentId + '/reply',
+      method: 'POST',
+      data: {
+        comment: {
+          body: comment
         }
-      }).then(res => {
-        resolve(res);
-      }).catch(err => {
-        reject(err);
-      })
+      }
+    }).then(res => {
+      resolve(res);
+    }).catch(err => {
+      reject(err);
     })
   })
 }
@@ -291,34 +238,28 @@ const replyComment = (commentId, comment) => {
 
 const fetchSurvey = (survey_name) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + '/v1/report_posts/survey',
-        header: header,
-        method: 'get',
-        data: { survey_name: survey_name }
-      }).then(res => {
-        resolve(res.data);
-      }).catch(err => {
-        reject(err);
-      })
+    request({
+      url: BASE_URL + '/v1/report_posts/survey',
+      method: 'get',
+      data: { survey_name: survey_name }
+    }).then(res => {
+      resolve(res.data);
+    }).catch(err => {
+      reject(err);
     })
   })
 }
 
 const sendSurvey = (data) => {
   return new Promise((resolve, reject) => {
-    fetchTokenHeader().then((header) => {
-      request({
-        url: BASE_URL + '/v1/report_posts',
-        header: header,
-        method: 'POST',
-        data: data
-      }).then(res => {
-        resolve(res.data);
-      }).catch(err => {
-        reject(err);
-      })
+    request({
+      url: BASE_URL + '/v1/report_posts',
+      method: 'POST',
+      data: data
+    }).then(res => {
+      resolve(res.data);
+    }).catch(err => {
+      reject(err);
     })
   })
 }
--- src/common/utils.js
+++ src/common/utils.js
@@ -1,6 +1,33 @@
 import eventHub from './eventHub';
 import wepy from '@wepy/core';
 
+const MAXRETRY = 3;
+
+const fetchTokenHeader = () =>{
+  let data = wx.getStorageSync('accessToken')
+  if (!data){
+    eventHub.$emit('auth-user', err);
+    return false
+  }
+  return data
+};
+
+const requestHeader = (authRequire = true) => {
+  return new Promise((resolve, reject) => {
+    let header = {
+      'content-type': 'application/json',
+      'Accept':'*/*'
+    }
+
+    if (authRequire == true) {
+      let authHeader = fetchTokenHeader()
+      authHeader ? resolve({ ...header, 'Authorization': authHeader }) : reject({error: 'Token not found'})
+    } else {
+      resolve(header);
+    }
+  })
+}
+
 const numberValidator = (min, max) =>{
   return (v) => {
     let valid = true, num = Number(v);
@@ -25,29 +52,37 @@ const stringToBoolean = () =>{
   };
 };
 
-const request = (options) => {
+const request = (options, authRequire = true) => {
   return new Promise((resolve, reject) => {
-    wepy.wx.request({ ...options }).then((res) => {
-      errorHanlder(res).then((res) => {
-        resolve(res)
-      }).catch(err => {
-        // retry if authentication failed
-        setTimeout(function() {
-          wepy.wx.request({ ...options }).then((res) => {
-            errorHanlder(res).then(res => {
-              resolve(res);
-            }).catch(err => {
-              reject(err);
-            })
-          })
-        }, 5000)
-      })
+    sendRequest(options, authRequire).then( (res) => {
+      resolve(res)
     }).catch((err) => {
       eventHub.$emit('wx-error', err);
       reject(err)
     })
   });
 };
+
+
+const sendRequest = (options, authRequire, retryCount = 0) => {
+  return new Promise((resolve, reject) => {
+    if (retryCount == MAXRETRY) {
+      reject('Reach Max Retry Times')
+    } else {
+      setTimeout(function(){
+        requestHeader(authRequire).then( headers => {
+          wepy.wx.request({ ...options, header: headers }).then( res => {
+            errorHanlder(res).then(res => resolve(res)).catch( err => {
+              sendRequest(options, true, retryCount + 1).then(res => resolve(res)).catch((err) => {
+                reject(err)
+              })
+            })
+          }).catch(err => eventHub.$emit('wx-error', err))
+        }).catch(err => eventHub.$emit('wx-error', err))
+      }, retryCount * 2000);
+    }
+  })
+}
 
 const errorHanlder = (res) => {
   return new Promise((resolve, reject) => {
